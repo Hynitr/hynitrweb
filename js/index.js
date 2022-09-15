fd31f25f55084f3a06dc292ec32e74c4482b395b
rhose1234 <marvelousrhose@gmail.com>
2022-09-15 16:22:19 +0100

    updt

--- js/index.js
+++ js/index.js
@@ -17,4 +17,12 @@ $(document).ready(function(){
 
        
     })
-  })
+  });
+  /*---scrolling up------*/
+
+  let mybutton = document.getElementById("myBtn");
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
